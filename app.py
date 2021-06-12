f392abbc2afff335c136d3801331a949519f9e9b
Jens Berntsen <jensbern@kth.se>
2021-06-12 13:09:35 +0200

    Flask server init

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path="",
+            static_folder="web/static", template_folder="web/templates")
+
+
+@app.route("/")
+def index():
+  return render_template("index.html")
---
+++ web/static/js/components/components.js
@@ -0,0 +1,5 @@
+// import { Champion } from "./champion.js"
+// import { Item } from "./item.js"
+
+// customElements.define("champion-selector", Champion)
+// customElements.define("item-selector", Item)
