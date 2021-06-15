5062d99996023518dd26206d42d21ea1a1c015c5
Jens Berntsen <jensbern@kth.se>
2021-06-15 17:43:28 +0200

    Update user component style and data flow

--- web/static/js/components/user.js
+++ web/static/js/components/user.js
@@ -1,27 +1,72 @@
 import { baseTemplate } from "./template.js";
 
+// GraphQL Query
+// {
+//   allStaff {
+//     edges {
+//       node {
+//         name
+//         contactInfo
+//       }
+//     }
+//   }
+// }
+const data = {
+  "data": {
+    "allStaff": {
+      "edges": [
+        {
+          "node": {
+            "name": "Pelle",
+            "contactInfo": "pelle#1234"
+          }
+        },
+        {
+          "node": {
+            "name": "Lisa",
+            "contactInfo": "lisa.s@gmail.com"
+          }
+        }
+      ]
+    }
+  }
+}
+
 export class User extends HTMLElement {
   constructor() {
     super();
-    const shadowRoot = this.attachShadow({mode: "open"})
-    shadowRoot.append(baseTemplate.content.cloneNode(true));
+    this.attachShadow({mode: "open"})
+    this.shadowRoot.append(baseTemplate.content.cloneNode(true));
+    const STYLE = document.createElement("style");
+    STYLE.innerText = `
+      h3 {
+        padding-top: 8px;
+        font-size: 1.4em;
+      }
+      p {
+        font-size: 1.2em;
 
-    const username = this.getAttribute("username");
+      }
+     
+    `
     const userid = this.getAttribute("userid");
-
+    console.log(userid);
+    this.shadowRoot.append(STYLE);
+  }
+  
+  displayUserData (userData) {
+    const H3_name = document.createElement("h3");
+    H3_name.innerText = userData.name;
+    this.shadowRoot.append(H3_name);
+  
+    const P_contactInfo = document.createElement("p");
+    P_contactInfo.innerText = userData.contactInfo;
+    this.shadowRoot.append(P_contactInfo);
     
-
-    const H3_username = document.createElement("h3");
-    H3_username.innerText = username;
-    shadowRoot.append(H3_username);
-
-    const H3_contactInfo = document.createElement("h3");
-    H3_contactInfo.innerText = userid;
-    shadowRoot.append(H3_contactInfo);
   }
 
   connectedCallback() {
-
+    this.displayUserData(data.data.allStaff.edges[0].node)
   }
 
   disconnectedCallback() {
