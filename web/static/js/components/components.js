db2c4013091f883a1351135273df7ee1e6b524d1
jensbern <jensbern@kth.se>
2021-06-30 21:02:45 +0200

    cleanup unused files

--- web/static/js/components/components.js
+++ web/static/js/components/components.js
@@ -1,13 +1,9 @@
 import {User} from "./user.js";
 import {Skills} from "./skills.js";
 import {Experiences} from "./experiences.js";
-// import {CreateSkill} from "./createSkill.js";
-import {CreateExperience} from "./createExperience.js"
 import {CreateUser} from "./createUser.js"
 
 customElements.define("experiences-element", Experiences);
 customElements.define("skills-element", Skills);
 customElements.define("user-element", User);
-// customElements.define("createskill-element", CreateSkill);
-customElements.define("createexperience-element", CreateExperience);
 customElements.define("createuser-element", CreateUser);
