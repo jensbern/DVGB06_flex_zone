c208f31a4ce568f2521b7e2a947a959e837ce2cc
Jens Berntsen <jensbern@kth.se>
2021-07-23 15:54:14 +0200

    search skills and experiences

--- web/static/js/components/search.js
+++ web/static/js/components/search.js
@@ -43,7 +43,7 @@ export class Search extends HTMLElement {
         background-color: rgb(252, 252, 252);
         border: 1px solid lightgrey;
       }
-      input[type="text"], input[type="password"]{
+      input[type="text"], select{
         
         margin: 8px;
         padding: 8px;
@@ -66,31 +66,45 @@ export class Search extends HTMLElement {
 
       }
       label{
-        display:none;
+        // display:none;
         margin: 8px;
         margin-right: 0px;
         padding: 8px;
+      }
+      .search_forms{
+        display:grid;
+        grid-template-columns: 1fr 1fr;
+      }
+      .selected {
+        border: 2px solid black;
       }
     `;
     this.shadowRoot.append(STYLE);
   }
 
-  displaySearch = () => {
+  selectedFORM = (FORM) => {
+    const FORMS = this.shadowRoot.querySelectorAll(".selected");
+    for (let i = 0; i < FORMS.length; i++) {
+      FORMS[i].classList.remove("selected");
+    }
+    FORM.classList.add("selected");
+  };
+
+  displayGenericSearch = (root) => {
     const FORM = document.createElement("form");
 
     const P_search = document.createElement("p");
-    const LABEL_search = document.createElement("label")
-    LABEL_search.innerText = "Search";
-    LABEL_search.setAttribute("for", "search")
+    const LABEL_search = document.createElement("label");
+    LABEL_search.innerText = "Generic Search";
+    LABEL_search.setAttribute("for", "search");
     const INPUT_search = document.createElement("input");
     INPUT_search.setAttribute("type", "text");
     INPUT_search.setAttribute("id", "search");
     INPUT_search.setAttribute("name", "search");
     INPUT_search.required = true;
     INPUT_search.setAttribute("placeholder", "e.g. Username or name...");
-    P_search.append(LABEL_search, INPUT_search, "*")
+    P_search.append(LABEL_search, INPUT_search, "*");
     FORM.append(P_search);
-
 
     const P_submit = document.createElement("p");
     const LABEL_submit = document.createElement("label");
@@ -99,29 +113,117 @@ export class Search extends HTMLElement {
     const INPUT_submit = document.createElement("input");
     INPUT_submit.setAttribute("type", "submit");
     INPUT_submit.setAttribute("id", "submit");
-    INPUT_submit.value = "Search"
-    INPUT_submit.addEventListener("click", e => {
-      this.handleSubmit(e)
-    })
+    INPUT_submit.value = "Search";
+    INPUT_submit.addEventListener("click", (e) => {
+      this.handleSubmit(e, FORM, () => {
+        this.selectedFORM(FORM);
+        this.genericSearch(FORM);
+      });
+    });
     P_submit.append(LABEL_submit, INPUT_submit);
     FORM.append(P_submit);
-    this.shadowRoot.append(FORM);
-  }
+    root.append(FORM);
+  };
 
-  handleSubmit = (e) => {
+  displaySkillSearch = (root) => {
+    const FORM = document.createElement("form");
+
+    const P_search = document.createElement("p");
+    const LABEL_search = document.createElement("label");
+    LABEL_search.innerText = "Skill Search";
+    LABEL_search.setAttribute("for", "skill_search");
+    const INPUT_search = document.createElement("input");
+    INPUT_search.setAttribute("type", "text");
+    INPUT_search.setAttribute("id", "skill_search");
+    INPUT_search.setAttribute("name", "skill_search");
+    INPUT_search.required = true;
+    INPUT_search.setAttribute("placeholder", "e.g. Pick ban or Top lane...");
+    P_search.append(LABEL_search, INPUT_search, "*");
+    FORM.append(P_search);
+
+    const P_submit = document.createElement("p");
+    const LABEL_submit = document.createElement("label");
+    LABEL_submit.innerText = "Submit";
+    LABEL_submit.setAttribute("for", "submit");
+    const INPUT_submit = document.createElement("input");
+    INPUT_submit.setAttribute("type", "submit");
+    INPUT_submit.setAttribute("id", "skill_submit");
+    INPUT_submit.value = "Search";
+    INPUT_submit.addEventListener("click", (e) => {
+      this.handleSubmit(e, FORM, () => {
+        this.selectedFORM(FORM);
+        this.skillSearch(FORM);
+      });
+    });
+    P_submit.append(LABEL_submit, INPUT_submit);
+    FORM.append(P_submit);
+    root.append(FORM);
+  };
+
+  displayExperienceSearch = (root) => {
+    const FORM = document.createElement("form");
+
+    const P_search = document.createElement("p");
+    const LABEL_search = document.createElement("label");
+    LABEL_search.innerText = "Experience Search";
+    LABEL_search.setAttribute("for", "experience_search");
+    const SELECT_search = document.createElement("select");
+    SELECT_search.setAttribute("id", "experience_search");
+    SELECT_search.setAttribute("name", "experience_search");
+    SELECT_search.required = true;
+
+    var OPTION_type = document.createElement("option");
+    OPTION_type.innerText = "Select Type";
+    OPTION_type.setAttribute("value", "");
+    SELECT_search.append(OPTION_type);
+    const options = [
+      "Analyst",
+      "Coach",
+      "Positional Coach",
+      "Sports Psychologist",
+      "Other",
+    ];
+    for (let i = 0; i < options.length; i++) {
+      var OPTION_type = document.createElement("option");
+      OPTION_type.innerText = options[i];
+      OPTION_type.setAttribute("value", options[i].toLowerCase());
+      SELECT_search.append(OPTION_type);
+    }
+    P_search.append(LABEL_search, SELECT_search, "*");
+    FORM.append(P_search);
+
+    const P_submit = document.createElement("p");
+    const LABEL_submit = document.createElement("label");
+    LABEL_submit.innerText = "Submit";
+    LABEL_submit.setAttribute("for", "submit");
+    const INPUT_submit = document.createElement("input");
+    INPUT_submit.setAttribute("type", "submit");
+    INPUT_submit.setAttribute("id", "submit");
+    INPUT_submit.value = "Search";
+    INPUT_submit.addEventListener("click", (e) => {
+      this.handleSubmit(e, FORM, () => {
+        this.selectedFORM(FORM);
+        this.experienceSearch(FORM);
+      });
+    });
+    P_submit.append(LABEL_submit, INPUT_submit);
+    FORM.append(P_submit);
+    root.append(FORM);
+  };
+
+  handleSubmit = (e, root, callback) => {
     var isValid = true;
-    const REQUIRED_ELEMENTS = this.shadowRoot.querySelectorAll("[required]");
+    const REQUIRED_ELEMENTS = root.querySelectorAll("[required]");
     for (let i = 0; i < REQUIRED_ELEMENTS.length; i++) {
       isValid = isValid && !!REQUIRED_ELEMENTS[i].value;
     }
     if (isValid) {
       e.preventDefault();
-      this.search();
+      callback();
     }
   };
-  search = () => {
-    const FORM = this.shadowRoot.querySelector("form");
-    const formData = new FormData(FORM)
+  genericSearch = (FORM) => {
+    const formData = new FormData(FORM);
     fetch("/graphql", {
       method: "POST",
       headers: {
@@ -141,6 +243,7 @@ export class Search extends HTMLElement {
               ... on Experience {
                 type
                 description
+                at
                 staff {
                   name
                   username
@@ -166,37 +269,126 @@ export class Search extends HTMLElement {
         return resp.json();
       })
       .then((data) => {
-        this.displaySearchData(data.data.search);
+        this.displayGenericSearchData(data.data.search);
       });
-    }
-    
-  displaySearchData = (data) => {
+  };
+
+  skillSearch = (FORM) => {
+    const formData = new FormData(FORM);
+    fetch("/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+        query skills($type: String) {
+          skills(name: $type) {
+            name
+            description
+            staff {
+              name
+              username
+            }
+          }
+        }
+        `,
+        variables: {
+          type: formData.get("skill_search"),
+        },
+      }),
+    })
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        this.displaySearchData(data.data.skills);
+      });
+  };
+  experienceSearch = (FORM) => {
+    const formData = new FormData(FORM);
+    fetch("/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+          query experiences($type: String) {
+            experiences(experienceType: $type) {
+              type
+              description
+              at
+              staff {
+                name
+                username
+              }
+            }
+          }
+        `,
+        variables: {
+          type: formData.get("experience_search"),
+        },
+      }),
+    })
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        this.displaySearchData(data.data.experiences);
+      });
+  };
+
+  displayGenericSearchData = (data) => {
     const SECTION_search = this.shadowRoot.querySelector("#search_result");
     SECTION_search.innerHTML = "";
-    if(!data.length){
+    if (!data.length) {
       const H3_error = document.createElement("h3");
       H3_error.innerText = "No result found";
       SECTION_search.append(H3_error);
       return;
     }
-    
+
     var currentType = "";
     var ARTICLE_current = document.createElement("article");
-    for(let i = 0; i < data.length; i++){
-      if(currentType == data[i]["__typename"]){
+    for (let i = 0; i < data.length; i++) {
+      if (currentType == data[i]["__typename"]) {
         ARTICLE_current.append(this.createSearchDataElement(data[i]));
       } else {
-        currentType = data[i]["__typename"];
+        if (data[i]["__typename"]) {
+          currentType = data[i]["__typename"];
+        } else {
+          console.log(data[i]);
+          currentType = data[i].type;
+        }
         ARTICLE_current = document.createElement("article");
-        const H3_type = document.createElement("h3")
+        const H3_type = document.createElement("h3");
         H3_type.innerText = currentType;
         ARTICLE_current.append(H3_type);
         ARTICLE_current.append(this.createSearchDataElement(data[i]));
-        
+
         SECTION_search.append(ARTICLE_current);
       }
     }
-  }
+  };
+
+  displaySearchData = (data) => {
+    const SECTION_search = this.shadowRoot.querySelector("#search_result");
+    SECTION_search.innerHTML = "";
+    if (!data.length) {
+      const H3_error = document.createElement("h3");
+      H3_error.innerText = "No result found";
+      SECTION_search.append(H3_error);
+      return;
+    }
+    var ARTICLE_current = document.createElement("article");
+    const H3_type = document.createElement("h3");
+    H3_type.innerText = data[0].type;
+    for (let i = 0; i < data.length; i++) {
+      ARTICLE_current.append(this.createSearchDataElement(data[i]));
+    }
+    SECTION_search.append(ARTICLE_current);
+  };
 
   createSearchDataElement = (data) => {
     const SECTION = document.createElement("section");
@@ -212,16 +404,16 @@ export class Search extends HTMLElement {
         A_name.innerText = data.name;
         A_name.href = `/user/${data.username}`;
         let P_contact = document.createElement("p");
-        P_contact.innerText = `${data.contactInfo} [${data.contactType}]`
-        SECTION.append(P_contact)
+        P_contact.innerText = `${data.contactInfo} [${data.contactType}]`;
+        SECTION.append(P_contact);
         break;
 
       case "Experience":
-        A_name.innerText = data.staff.name
-        A_name.href = `/user/${data.staff.username}`
-        H4_name.append(A_name)
-        SECTION.append(H4_name)
-        P_type.innerText = data.type;
+        A_name.innerText = data.staff.name;
+        A_name.href = `/user/${data.staff.username}`;
+        H4_name.append(A_name);
+        SECTION.append(H4_name);
+        P_type.innerText = data.type + " - " + data.at;
         P_description.innerText = data.description;
         SECTION.append(P_type, P_description);
         break;
@@ -237,11 +429,20 @@ export class Search extends HTMLElement {
         break;
 
       default:
+        A_name.innerText = data.staff.name;
+        A_name.href = `/user/${data.staff.username}`;
+        H4_name.append(A_name);
+        SECTION.append(H4_name);
+        P_type.innerText = data.name ? data.name : data.type;
+        if (data.at) {
+          P_type.innerText += " - " + data.at;
+        }
+        P_description.innerText = data.description;
+        SECTION.append(P_type, P_description);
         break;
-      
     }
     return SECTION;
-  }
+  };
 
   addToolTip = (DOM, text) => {
     const TOOLTIP = document.createElement("div");
@@ -264,12 +465,15 @@ export class Search extends HTMLElement {
     window.addEventListener("click", click_func);
   };
 
-  
   connectedCallback() {
-    this.displaySearch()
-    const SECTION_search_result = document.createElement("section")
-    SECTION_search_result.setAttribute("id", "search_result")
-    this.shadowRoot.append(SECTION_search_result);
+    const SECTION_search = document.createElement("section");
+    SECTION_search.className = "search_forms";
+    this.displayGenericSearch(SECTION_search);
+    this.displayExperienceSearch(SECTION_search);
+    this.displaySkillSearch(SECTION_search);
+    const SECTION_search_result = document.createElement("section");
+    SECTION_search_result.setAttribute("id", "search_result");
+    this.shadowRoot.append(SECTION_search, SECTION_search_result);
   }
 
   disconnectedCallback() {}
